77c7083ca337fc48b4c1a4617d7342ad5dbf36a7
Suraj-ShahA12 <i24ai012@aid.svnit.ac.in>
2024-09-03 05:03:05 -0700

    Update Assignment-19.c

--- Assignment-19.c
+++ Assignment-19.c
@@ -4,4 +4,5 @@ int main() {
 	printf("Enter a character: ");
 	scanf("%c",&ch);
 	ch>=97 && ch<=122 ? printf("%c is a small case letter",ch) : printf("%c is  not a small case letter",ch);
+	return 0;
 }
